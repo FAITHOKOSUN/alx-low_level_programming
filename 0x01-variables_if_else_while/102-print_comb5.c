8105f8942c2556ddf6db34922d537095b4e34979
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-03 16:46:51 -0700

    combo 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,6 +12,9 @@ for (num1 = 0; num1 <= 98; num1++)
 for (num1 = num2 + 1; num2 <= 99; num2++)
 {
 putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
 if (num1 == 98 && num2 == 99)
 continue;

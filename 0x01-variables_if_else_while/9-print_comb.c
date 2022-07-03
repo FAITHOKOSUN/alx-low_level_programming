0ab28eb215f5e490c2d83dedcb47f4d2314735c4
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-03 15:18:09 -0700

    base

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - Entry point 
+* main - Entry point
 *
 * Return: Always 0 (Success)
 */
@@ -10,7 +10,7 @@ int num;
 for (num = 0; num <= 9; num++)
 {
 putchar((num % 10) + '0');
-if(num == 9)
+if (num == 9)
 continue;
 putchar(',');
 putchar(' ');

25a6c1fae07896131bf3204a11ae15ec035152b5
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-05 18:37:28 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints 10 times alphabet
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+char a, b;
+for (i = 0; i <= 9; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}

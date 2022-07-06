68223828540ecc36125186d68d3cc84f4be01cef
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-05 18:12:53 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
-* print_alphabet -function to print abc
+* print_alphabet - function to print abc
 *
-* Description: use this prototype  function print_alphabet
-*
-* Return: Always 0.
+* Return: 0
 */
 void print_alphabet(void)
 {

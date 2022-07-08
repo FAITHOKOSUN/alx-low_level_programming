2313f805583d78504a48767aa2db1927a3a1c488
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-08 09:59:32 -0700

    task 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,14 @@
 
 void print_most_numbers(void)
 {
-int x;
-for (x = 0; x < 10; x++)
+char x = 0;
+while (x <= 9)
 {
-if (x !=2 && x !=4)
-_putchar(x + '0');
+if (x != 2 && x != 4)
+{
+_putchar('0' + x);
+}
+x++;
 }
 _putchar('\n');
 }

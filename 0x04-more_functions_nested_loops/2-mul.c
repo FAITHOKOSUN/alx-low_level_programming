99527f0e5bc19e1624441cbddf3bd75ec24439a7
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-08 08:56:54 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
 * mul - returns the integer of the result
 * @a: int parameter1
 * @b: int parameter2
 * Return: Always sucess
 */
+
 int mul(int a, int b)
 {
-return (mul);
+return (a * b);
 }

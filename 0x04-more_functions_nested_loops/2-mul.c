adae38295b2286132fc3482a79932e9c3286f34d
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-07 20:29:03 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - returns the integer of the result
+* @a: int parameter1
+* @b: int parameter2
+* Return: Always sucess
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,1 +1,13 @@
-int _isupper(int c)
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+print_numbers(void);
+print_most_numbers(void);
+
+#endif /* MAIN_H */

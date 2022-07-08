cd426bd9fcd94954e33b618c7b39fb2bd9bef70e
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-08 08:49:20 -0700

    task 2

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,8 +6,8 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b);
-int print_numbers(void);
-int print_most_numbers(void);
+int mul(int a,int b);
+void print_niumbers(void);
+void print_most_numbers(void);
 
 #endif /* MAIN_H */

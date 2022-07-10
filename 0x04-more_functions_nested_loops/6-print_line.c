b27a88d64e96022a8b90930a191d275dedb93d29
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-10 06:19:54 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,2 +1,20 @@
-nclude "main.h"
+#include "main.h"
+/**
+ * print_line - to print characters
+ * @n:  integer to print number of -
+ * Return : void
+ *
+ */
 
+void print_line(int n)
+
+{
+	int i = 0;
+
+	while (i < n && i > n)
+	{
+		_putchar('-');
+		i++;
+	}
+	_putchar('\n');
+}

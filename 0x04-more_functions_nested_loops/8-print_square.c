e8057dfbe3975579ae93b7a5de25a3e9b5d9cc7f
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-11 01:55:47 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,25 +2,22 @@
 
 /**
  * print_square - a function that prints a square
- * @n: is the size of the square
+ * @size: is the size of the square
  */
 
 void print_square(int size)
 {
 	int i, j;
 
-	if (i <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-        for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < i; j++)
-		{
+		for (j = 0; j < size; j++)
 			_putchar('#');
-			_putchar('\n'); 
-		}
-		
+		_putchar('\n');
 	}
 }

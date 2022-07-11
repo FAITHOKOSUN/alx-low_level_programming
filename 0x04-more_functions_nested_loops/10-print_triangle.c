ffe07fe30f2a6a73bdb4378389f33f81624a4026
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-11 06:05:54 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - a function that prints a triangle
+ * @size: to print the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i = 1; j;
+
+	while (i <= size && size > 0)
+	{
+		j = 0;
+		while (j < size - i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		j = 0;
+		while (j < i)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar("\n");
+		i++;
+	}
+	if ( i == j)
+		_putchar('\n');
+}	

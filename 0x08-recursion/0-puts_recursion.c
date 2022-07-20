9695a1893bcc28843488db0045fc4c0e1803a1ad
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-20 14:25:29 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,17 @@
  * @s: string to print
  *
  * Return: void
- */
-
+*/
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		return;
 	}
-	putchar(*s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
+	else
+	{
+		_putchar('\n')
+	}
 }

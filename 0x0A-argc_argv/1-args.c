0290d13d386c3d1a8c54a70294137d6c4aa7b8b6
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-23 15:49:35 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,13 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
 {
 	int a;
 
 	for (a = 0; a < argc; a++)
 	{
-		printf("%s\n" argv[i]);
+		printf("%s\n", argv[a]);
 	}
 	return (0);
 }	     	

a6a698d9bcce23626efbd9e09d4dfaa3b2ed25cb
FAITHOKOSUN <okosunfaith2@gmail.com>
2022-07-23 15:39:57 -0700

    task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: counts argument 
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[]);
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n" argv[i]);
+	}
+	return (0);
+}	     	
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+ It ain't what they call you, it's what you answer to
+ Silence is argument carried out by other means
+Write a program that prints all arguments it receives.
